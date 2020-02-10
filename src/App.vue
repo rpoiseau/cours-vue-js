7b1f1c48a641d88f184857d2c6114ab71634a59a
Romain Poiseau <>
2020-02-10 18:36:26 +0100

    feat(component): component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <my-component
-      :firstName="firstName"
-      last-name="Poiseau"></my-component>
+    <my-component></my-component>
   </div>
 </template>
 
@@ -16,7 +14,6 @@ export default {
   },
   data() {
     return {
-      firstName: "Romain"
     }
   }
 }
