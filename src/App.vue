f98aa5923156079a913ad7be5c7fde1f0d4cdb9f
Romain Poiseau <>
2020-02-10 18:04:31 +0100

    feat(fundamentals): adding fundamentals examples

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Fundamentals/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Fundamentals from "./components/Fundamentals";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Fundamentals
   }
 }
 </script>
